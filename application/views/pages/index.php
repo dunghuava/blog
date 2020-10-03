b7127bb2affe099359fc37a29d047e87f190beba
hứa văn dũng <60870815+dunghuava@users.noreply.github.com>
2020-10-03 15:12:10 +0700

    trang chủ index

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -9,9 +9,9 @@
                     <div class="col-md-6">
                         <h2>
                             CRM Contact Center <br>
-                            Case management  - Quản lý chăm sóc khách hàng đa kênh hợp nhất <br>
-                            Hợp nhất đa kênh omni-channel như thoại, email, website, live chat, sms, social media (facebook, zalo,...) tại một giao diện duy nhất.    
                         </h2>
+                        <h3>Case management  - Quản lý chăm sóc khách hàng đa kênh hợp nhất <br>
+                            Hợp nhất đa kênh omni-channel như thoại, email, website, live chat, sms, social media (facebook, zalo,...) tại một giao diện duy nhất.    </h3>
                     </div>
                 </div>
             </div>
@@ -25,8 +25,167 @@
                     <div class="col-md-6">
                         <h2>
                             CRM Contact Center <br>
-                            Giúp nâng cao hiệu suất làm việc của nhân viên telesales với chiến dịch quy số tự động Predictive Dialer
                         </h2>
+                        <h3> Giúp nâng cao hiệu suất làm việc của nhân viên telesales với chiến dịch quy số tự động Predictive Dialer</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="over-section" id="over-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="item-over">
+                    <p><img src="<?=base_url('upload/icon1.png')?>" alt=""></p>
+                    <p>Dịch vụ Mixci là gì ?</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="item-over">
+                    <p><img src="<?=base_url('upload/icon2.png')?>" alt=""></p>
+                    <p>Đối tượng sử dụng Mixci</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="item-over">
+                    <p><img src="<?=base_url('upload/icon3.png')?>" alt=""></p>
+                    <p>Tại sao nên chọn Mixci ?</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="item-over">
+                    <p><img src="<?=base_url('upload/icon4.png')?>" alt=""></p>
+                    <p>Khách hàng của Mixci</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sec-content">
+    <div class="container">
+        <h3 class="main-title">Dịch vụ MiCXM là gì ?</h3>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p>MiCXM hay còn gọi là Contact Center là hệ thống tổng đài chăm sóc khách hàng, bán hàng được tích hợp qua nhiều kênh như thoại, live chat, email, sms, facebook…..tại một nơi duy nhất. Đây được xem là công cụ liên kết đa chiều giữa doanh nghiệp và khách hàng giúp tiếp cận, chăm sóc khách hàng một cách hiệu quả và nâng cao tính trải nghiệm của người dùng.</p>
+                <br><p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/gssduBBhbc4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sec-content bg-blue">
+    <div class="container">
+        <h3 class="main-title">Đối tượng sử dụng MiCXM</h3>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p>Đối tượng sử dụng dịch vụ MiCXM là các doanh nghiệp, các tổ chức, các cửa hàng, siêu thị,… có thể phân loại theo mục đích sử dụng như sau</p>
+                <br>
+            </div>
+            <?php for ($i=0;$i<=7;$i++){ ?>
+            <div class="col-md-3">
+                <div class="item-view">
+                    <p><img src="<?=base_url('upload/icon5.png')?>" alt=""></p>
+                    <h4><b>Telesales</b></h4>
+                    <p>Ngân hàng, bảo hiểm, tài chính, giáo dục và hầu hết các loại hình doanh nghiệp khác muốn tìm kiếm khách hàng qua điện thoại</p>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+</section>
+<section class="sec-content">
+    <div class="container">
+        <h3 class="main-title">Tại sao nên chọn MiCXM</h3>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p>Đối tượng sử dụng dịch vụ MiCXM là các doanh nghiệp, các tổ chức, các cửa hàng, siêu thị,… có thể phân loại theo mục đích sử dụng như sau</p>
+                <br>
+            </div>
+            <?php for ($i=0;$i<=7;$i++){ ?>
+            <div class="col-md-3">
+                <div class="item-view">
+                    <p><img src="<?=base_url('upload/icon6.png')?>" alt=""></p>
+                    <h4><b>Telesales</b></h4>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+</section>
+<section class="sec-content bg-blue">
+    <div class="container">
+        <h3 class="main-title">Những câu hỏi thường gặp</h3><br>
+        <div class="row">
+            <?php for ($i=0;$i<=9;$i++){ ?>
+                <div class="q">
+                    <div class="col-md-6">
+                        <div class="item-question">
+                            <div class="q-border">
+                                <p><span class="q-btn">+</span>&nbsp;<?=$i?>.Contact Center sử dụng cho nhóm khách hàng nào?</p>
+                            </div>
+                            <div class="item-anwser">
+                                <p>Gói chức năng Contact Center dành cho tất cả các doanh nghiệp muốn xây dựng trung tâm Chăm sóc khách hàng với đầy đủ các tính năng mạnh mẽ và phù hợp nhất. Có thêm các tính năng live chat, email, facebook, campaign hỗ trợ doanh nghiệp chăm sóc khách hàng tốt hơn và thuận tiện hơn.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</section>
+<section class="sec-content">
+    <div class="container">
+        <h3 class="main-title">Khách hàng của MiCXM</h3><br>
+        <div class="row">
+            <div class="slick-cus">
+                <?php for ($i=0;$i<=12;$i++){ ?>
+                <div class="col-md-2">
+                    <div class="item-small-slide">
+                        <a href="">
+                            <img src="<?=base_url('upload/icon7.png')?>" alt="">
+                        </a>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sec-content bg-blue">
+    <div class="container">
+        <h3 class="main-title">Phản hồi của khách hàng</h3><br>
+        <div class="row">
+            <div class="slick-feedback">
+                <?php for ($i=0;$i<=12;$i++){ ?>
+                <div class="col-md-12">
+                    <div class="item-feedback">
+                        <blockquote>
+                           <p> Hệ thống có đầy đủ tính năng, dễ dàng sử dụng giao diện trực quan. Có thể phân tầng theo từng lớp : Admin, Senior, Staff, Reporter, QC,.... Có hỗ trợ phần knowledge base tích hợp trên CRM dễ dàng hỗ trợ cho Agent</p>
+                        </blockquote>
+                        <div class="item-cus">
+                            <img src="<?=base_url('upload/img.jpg')?>" alt="">
+                            <h3>Anh. Văn Dũng</h3>
+                            <p>Fullstack developper tại Tokyo</p>
+                            <a href="">>> Xem chi tiết</a>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sec-content">
+    <div class="container">
+        <h3 class="main-title">Liên hệ ngay để được tư vấn</h3><br>
+        <div class="row">
+            <div class="slick-cus">
+                <div class="text-center">
+                    <div class="btn-contact-now">
+                        <a href="tel:0383868205"><span class="fa fa-phone"></span>&nbsp;Liên hệ tư vấn</a>
                     </div>
                 </div>
             </div>
--- assets/js/web.js
+++ assets/js/web.js
@@ -8,4 +8,71 @@ $('.main-slider').slick({
     prevArrow: false,
     nextArrow: false,
     cssEase: 'linear'
-  })
+  });
+  $('.slick-feedback').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:false,
+    prevArrow: false,
+    nextArrow: false,
+  });
+  $('.slick-cus').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow:"<span class='btn-arrow btn-prev fa fa-angle-left'></span>",
+    nextArrow:"<span class='btn-arrow btn-next fa fa-angle-right'></span>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+$('.item-question').click(function (e) { 
+    e.preventDefault();
+    $(this).toggleClass('opened');
+    $(this).find('.item-anwser').slideToggle('400');
+    if ($(this).hasClass('opened')){
+      $(this).find('.q-btn').html('--');
+    }else{
+      $(this).find('.q-btn').html('+');
+    }
+});
+
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("over-section");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
