4d613f29812b3b94045d9e7706d8c387230988f4
Tr.Thuan <33859797+truongthuan97@users.noreply.github.com>
2020-10-14 18:13:14 +0700

    messeger facebook

--- application/views/pages/socical-network.php
+++ application/views/pages/socical-network.php
@@ -9,6 +9,10 @@
     .zalo-chat-widget{
         display:none;
     }
+
+    .fb_dialog_content{
+        display:none;
+    }
 </style>
 <!-- Modal -->
 <div id="callback" class="modal fade" role="dialog">
@@ -23,9 +27,20 @@
   </div>
 </div>
 <div id="socical_network">
-    <div title="Facebook" class="item">
+    <div onclick="facebook()" title="Facebook" class="item">
         <div class="icon">
             <span class="fa fa-facebook"></span>
+        </div>
+
+        <div id="fb-root"></div>
+       
+         <!-- Your Chat Plugin code -->
+        <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="107857070823040"
+        theme_color="#0C714B"
+        logged_in_greeting="Chat với chúng tôi hoặc gọi 19001238."
+        logged_out_greeting="Chat với chúng tôi hoặc gọi 19001238.">
         </div>
     </div>
     <div onclick="$('.zalo-chat-widget').toggle()" title="Zalo" class="item fa-zalo">
@@ -46,3 +61,26 @@
 </div>
 <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
 <script src="https://sp.zalo.me/plugins/sdk.js"></script>
+<script type="text/javascript">
+
+    window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v8.0'
+      });
+    };
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    FB.CustomerChat.hideDialog();
+    function facebook() {
+        FB.CustomerChat.showDialog();
+    }
+
+</script>
+
